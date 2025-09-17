54933bc58c42ca510300e3188611869ef7ad34e4
htshongany <htshongany@gmail.com>
2025-09-17 04:44:22 -0800

    Updates

---
+++ assets/Icon-BSY9YNvu.js
@@ -0,0 +1,1 @@
+import{j as a}from"./react-DPbnXjGw.js";import"./highlight.js-vNZFY2Ar.js";const i=({name:r,className:t="",ariaHidden:s=!0})=>a.jsx("i",{className:`${r} ${t}`,"aria-hidden":s});export{i as default};
---
+++ assets/validateApiKey-oADKpzmq.js
@@ -0,0 +1,1 @@
+async function t(e){try{return(await fetch("https://generativelanguage.googleapis.com/v1beta/models",{headers:{"x-goog-api-key":e}})).ok}catch{return!1}}export{t as validateApiKey};
